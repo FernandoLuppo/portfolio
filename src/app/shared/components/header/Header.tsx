c06a6cf080e299938afa83108108bbcc0494611f
Fernando Luppo <fernandoluppo2@gmail.com>
2023-09-17 13:43:35 -0300

    feat: Finished header scroll animation

--- src/app/shared/components/header/Header.tsx
+++ src/app/shared/components/header/Header.tsx
@@ -20,7 +20,9 @@ export const Header: React.FC<IProps> = ({ isScrolling }) => {
   return (
     <header
       className={`fixed top-0 w-full z-10 flex justify-between px-24 py-5 max-lg:flex-col max-md:px-5 ${
-        isMobile ? `flex-col bg-default` : `bg-transparent`
+        !isScrolling && !isMobile
+          ? `bg-transparent`
+          : `transition-all border-b-2 border-card-border bg-default max-lg:flex-col`
       }`}
     >
       <div
@@ -29,7 +31,12 @@ export const Header: React.FC<IProps> = ({ isScrolling }) => {
         }`}
       >
         <a href="#introduction" className="flex items-center gap-2">
-          <BsCodeSlash size={50} />
+          <BsCodeSlash
+            size={50}
+            className={`transition-all ${
+              isMobile || isScrolling ? `text-white` : `text-default`
+            }`}
+          />
           <h3 className="text-3xl font-lato font-semibold text-white">
             LuppoTW
           </h3>
@@ -50,7 +57,7 @@ export const Header: React.FC<IProps> = ({ isScrolling }) => {
       </div>
 
       <nav
-        className={` transition-all duration-70000 max-lg:overflow-hidden max-lg:w-full max-lg:fixed max-lg:left-0 max-lg:top-0 max-lg:px-28 max-lg:bg-default max-md:px-10 ${
+        className={`transition-all duration-70000 max-lg:overflow-hidden max-lg:w-full max-lg:fixed max-lg:left-0 max-lg:top-0 max-lg:px-28 max-lg:bg-default max-md:px-10 ${
           isMobile
             ? `min-h-full pt-32 max-lg:opacity-100`
             : `py-0 h-0 max-lg:opacity-0`
